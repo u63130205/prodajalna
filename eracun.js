50b6c412db07ff6e0e2e0fb9a82834d448b58efd
Rok Razgoršek <rr6789@student.uni-lj.si>
2016-05-08 16:26:45 +0000

    Zaključek dela na II. nalogi

--- eracun.js
+++ eracun.js
@@ -47,27 +47,31 @@ function davcnaStopnja(izvajalec, zanr) {
 
 // Prikaz seznama pesmi na strani
 streznik.get('/', function(zahteva, odgovor) {
-  pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
-          Artist.Name AS izvajalec, Track.UnitPrice * " +
-          razmerje_usd_eur + " AS cena, \
-          COUNT(InvoiceLine.InvoiceId) AS steviloProdaj, \
-          Genre.Name AS zanr \
-          FROM Track, Album, Artist, InvoiceLine, Genre \
-          WHERE Track.AlbumId = Album.AlbumId AND \
-          Artist.ArtistId = Album.ArtistId AND \
-          InvoiceLine.TrackId = Track.TrackId AND \
-          Track.GenreId = Genre.GenreId \
-          GROUP BY Track.TrackId \
-          ORDER BY steviloProdaj DESC, pesem ASC \
-          LIMIT 100", function(napaka, vrstice) {
-    if (napaka)
-      odgovor.sendStatus(500);
-    else {
-        for (var i=0; i<vrstice.length; i++)
-          vrstice[i].stopnja = davcnaStopnja(vrstice[i].izvajalec, vrstice[i].zanr);
-        odgovor.render('seznam', {seznamPesmi: vrstice});
-      }
-  })
+    if(zahteva.session.login == null){
+      odgovor.redirect('/prijava');
+    }else{
+      pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
+              Artist.Name AS izvajalec, Track.UnitPrice * " +
+              razmerje_usd_eur + " AS cena, \
+              COUNT(InvoiceLine.InvoiceId) AS steviloProdaj, \
+              Genre.Name AS zanr \
+              FROM Track, Album, Artist, InvoiceLine, Genre \
+              WHERE Track.AlbumId = Album.AlbumId AND \
+              Artist.ArtistId = Album.ArtistId AND \
+              InvoiceLine.TrackId = Track.TrackId AND \
+              Track.GenreId = Genre.GenreId \
+              GROUP BY Track.TrackId \
+              ORDER BY steviloProdaj DESC, pesem ASC \
+              LIMIT 100", function(napaka, vrstice) {
+        if (napaka)
+          odgovor.sendStatus(500);
+        else {
+            for (var i=0; i<vrstice.length; i++)
+              vrstice[i].stopnja = davcnaStopnja(vrstice[i].izvajalec, vrstice[i].zanr);
+            odgovor.render('seznam', {seznamPesmi: vrstice});
+          }
+      })
+    }
 })
 
 // Dodajanje oz. brisanje pesmi iz košarice
@@ -200,6 +204,7 @@ streznik.post('/prijava', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
+    zahteva.session.login = polja.seznamStrank;
     var napaka2 = false;
     try {
       var stmt = pb.prepare("\
@@ -239,6 +244,7 @@ streznik.post('/stranka', function(zahteva, odgovor) {
 
 // Odjava stranke
 streznik.post('/odjava', function(zahteva, odgovor) {
+    zahteva.session.login=null;
     odgovor.redirect('/prijava') 
 })
 
